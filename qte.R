417db0fcf234c59a00d02f143991ce4b827a6735
Carlos Murillo <59664837+cmurillo1291@users.noreply.github.com>
2026-06-11 19:53:57 +0200

    Add files via upload
    To complement the ATE estimated via TMLE, we conducted an additional analysis using quantile treatment effects (QTEs). Whereas the ATE summarizes the average effect across the population, QTEs estimate treatment effects at different points of the outcome distribution, thereby capturing heterogeneity in the impact of exercise across individuals with varying levels of healthcare expenditureWe estimated QTEs using the ci.qte function from qte package, which implements the propensity score reweighting approach proposed by Firpo 66. Propensity scores were estimated using a parametric logistic regression model for treatment conditional on the beforementioned covariates. No machine learning methods were used for propensity score estimation. Survey weights were normalized by dividing each weight by the mean survey weight, such that the normalized weights had a mean of one and supplied to the estimator through the weighting argument. The propensity score model itself was estimated without survey weights; however, the normalized survey weights were incorporated in the propensity-score reweighting step used to construct the counterfactual outcome distributions and estimate treatment effects. SE were computed using bootstrap resampling with 5000 iterations.

---
+++ qte.R
@@ -0,0 +1,41 @@
+#install.packages("qte")
+library(qte)
+
+set.seed(123)
+
+# select variables
+Y <- his_subset$cost1
+A <- his_subset$exercise
+W <- his_subset %>%
+  select(cost0, age,gender,mobility_restrictions_elderly, home_safer, year,
+         multimorbidity,overweight_obese,education_cat, fall_category, weight)
+
+# normalization weights
+weights_norm <- his_subset$weight / mean(his_subset$weight)
+jt.cia <- ci.qte(
+  formla  = Y ~ A,
+  x       = W,               
+  data    = data.frame(Y = Y, A = A, W),
+  w       = weights_norm,         
+  probs   = seq(0.05, 0.95, 0.05),
+  se      = TRUE,
+  iters   = 5000,             
+  method  = "logit"          
+)
+
+summary(jt.cia)
+
+tab <- data.frame(
+  tau       = jt.cia$probs,
+  QTE       = round(jt.cia$qte, 2),
+  Std.Error = round(jt.cia$qte.se, 2),
+  CI        = paste0(
+    "(",
+    round(jt.cia$qte.lower, 0),
+    ", ",
+    round(jt.cia$qte.upper, 0),
+    ")"
+  )
+)
+flextable(tab)
+ggqte(jt.cia)
